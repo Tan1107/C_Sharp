c08e5fc180db8736fe1fe2759e4ade9d6288fe6c
vatakudo <tannguyen1107.bku@gmail.com>
2020-07-23 23:40:30 +0700

    bai2

---
+++ code1/buoi2_cau_truc_lenh_co_ban/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi2_cau_truc_lenh_co_ban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Console.WriteLine("Hello World!");
+            Console.WriteLine("test xem the nao");
+         ///   Console.ReadKey();
+            /* comment trong class xem the nao */
+            // cai nay cung duoc nua ha/ duoc ban oi
+            Console.WriteLine("choi ky nha");
+        }
+    }
+}
+// comment 1 dong
+/// comment ben trong 
+/* connebt bay hay ne */
